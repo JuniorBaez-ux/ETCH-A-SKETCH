cafb605f4f55a6e4b5998b01baec9ec6cde52fc3
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-10-24 22:30:17 -0400

    Final Changes.

--- logic.js
+++ logic.js
@@ -1,17 +1,17 @@
 const container = document.getElementById("container");
 const item = document.querySelectorAll(".grid-item");
 const boton = document.getElementById("Resizebtn");
-const cleanbt = document.getElementById("cleanbtn");
 const defaultcolor = "ededed";
 const colord = "#333333";
 
 let color1 = defaultcolor;
 let color = colord;
+let itemes = item;
 
 function makeRows(rows, columns){
     container.style.setProperty("--grid-rows" , rows);
     container.style.setProperty("--grid-columns", columns);
-
+    
     for (let j = 0; j < (rows * columns); j++) {
         let cell = document.createElement("div");
         cell.addEventListener("mouseover", changeColor)
@@ -30,7 +30,7 @@ function changeColor(e) {
 }
 
 function defaultColor(e){
-    e.target.style.backgroundColor = color1;
+    e.target.style.backgroundColor = color1; 
 }
 
 //This code is in charge of getting the size 
@@ -40,7 +40,8 @@ boton.onclick = getSize;
 function getSize(e){
     e = window.prompt("Please enter the size of the grid (Only one dimension)");
     if (e <= 100) {
-     makeRows(e, e);   
+        defaultColor;
+        makeRows(e, e);   
     }else{
         window.alert("Value out of limits")
     }
